1c5c4f360d26b16f5e730fe2b520b2bb36cec336
Lucas Mallmann <lucasmallmann76@gmail.com>
2021-11-29 20:42:45 -0300

    feat: bcrypter compare adapter

--- src/infra/cryptography/bcrypt-adapter/bcrypter-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypter-adapter.ts
@@ -1,7 +1,8 @@
 import bcrypt from 'bcrypt'
 import { Hasher } from '@/services/protocols/criptography/hasher'
+import { HashComparer } from '@/services/protocols/criptography/hash-comparer'
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
   constructor (
     private readonly salt: number
   ) {}
@@ -9,5 +10,10 @@ export class BcryptAdapter implements Hasher {
   async hash (value: string): Promise<string> {
     const hashedValue = await bcrypt.hash(value, this.salt)
     return hashedValue
+  }
+
+  async compare (value: string, hash: string): Promise<boolean> {
+    const isValid = await bcrypt.compare(value, hash)
+    return isValid
   }
 }
--- src/tests/infra/criptography/bcrypt-adapter/bcrypter-adapter.spec.ts
+++ src/tests/infra/criptography/bcrypt-adapter/bcrypter-adapter.spec.ts
@@ -7,6 +7,10 @@ jest.mock('bcrypt', () => {
   return {
     async hash (): Promise<string> {
       return Promise.resolve('hashed_value')
+    },
+
+    async compare (): Promise<boolean> {
+      return Promise.resolve(true)
     }
   }
 })
@@ -28,6 +32,27 @@ describe('Bcrypt Adapter', () => {
     test('should return hashed value correctly on success', async () => {
       const hashedValue = await sut.hash('some_value')
       expect(hashedValue).toBe('hashed_value')
+    })
+  })
+
+  describe('compare()', () => {
+    test('should call bcrypt compare with correct values', async () => {
+      const compareSpy = jest.spyOn(bcrypt, 'compare')
+      await sut.compare('value', 'hashed_value')
+      expect(compareSpy).toHaveBeenCalledWith('value', 'hashed_value')
+    })
+
+    test('should return true if bcrypt.compare() returns true', async () => {
+      const isValid = await sut.compare('some_value', 'hash_value')
+      expect(isValid).toBe(true)
+    })
+
+    test('should return false if bcrypt.compare() returns false', async () => {
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => {
+        return Promise.resolve(false)
+      })
+      const isValid = await sut.compare('some_value', 'hash_value')
+      expect(isValid).toBe(false)
     })
   })
 
