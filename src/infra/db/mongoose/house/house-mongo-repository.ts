a60b0f8f2cc0bdf05c6d0ca6699390348c86264e
Lucas Mallmann <lucasmallmann76@gmail.com>
2022-03-02 23:28:07 -0300

    feat: mongo load houses

--- src/infra/db/mongoose/house/house-mongo-repository.ts
+++ src/infra/db/mongoose/house/house-mongo-repository.ts
@@ -3,10 +3,11 @@ import {
   AddHouseRepository,
   AddHouseRepositoryParams
 } from '@/services/protocols/db/house/add-house-repository'
+import { LoadHousesRepository } from '@/services/protocols/db/house/load-houses-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { HouseMongooseModel } from '../models/mongoose-house-model'
 
-export class HouseMongoRepository implements AddHouseRepository {
+export class HouseMongoRepository implements AddHouseRepository, LoadHousesRepository {
   async addHouse (house: AddHouseRepositoryParams): Promise<HouseModel> {
     const houseDocument = new HouseMongooseModel(house)
     const createdHouse = await houseDocument.save()
@@ -14,5 +15,10 @@ export class HouseMongoRepository implements AddHouseRepository {
       ...MongoHelper.withId(createdHouse.toObject()),
       images: createdHouse.getHouseImages()
     }
+  }
+
+  async loadAll (): Promise<HouseModel[]> {
+    const houses = await HouseMongooseModel.find()
+    return houses.map(house => MongoHelper.withId(house.toObject()))
   }
 }
--- src/tests/infra/db/mongoose/house/house-mongo-repository.spec.ts
+++ src/tests/infra/db/mongoose/house/house-mongo-repository.spec.ts
@@ -1,9 +1,59 @@
 import { HouseMongoRepository } from '@/infra/db/mongoose/house/house-mongo-repository'
 import { MongoHelper } from '@/infra/db/mongoose/helpers/mongo-helper'
 import { AccountMongooseModel } from '@/infra/db/mongoose/models/mongoose-account-model'
+import { HouseMongooseModel } from '@/infra/db/mongoose/models/mongoose-house-model'
 
 const makeSut = (): HouseMongoRepository => {
   return new HouseMongoRepository()
+}
+
+const makeFakeHouses = () => {
+  return [
+    {
+      name: 'any_name_1',
+      city: 'any_city_1',
+      location: {
+        type: 'Point',
+        coordinates: [0, 0]
+      },
+      state: 'any_state',
+      address: {
+        houseNumber: 0,
+        street: 'any_street',
+        zipCode: 'any_zipCode'
+      },
+      images: ['any_image'],
+      highlightImage: 'any_highlight_image',
+      createdAt: new Date(),
+      price: 100,
+      owner: {
+        email: 'any_email',
+        name: 'any_name'
+      }
+    },
+    {
+      name: 'any_name_2',
+      city: 'any_city_2',
+      location: {
+        type: 'Point',
+        coordinates: [0, 0]
+      },
+      state: 'any_state',
+      address: {
+        houseNumber: 0,
+        street: 'any_street',
+        zipCode: 'any_zipCode'
+      },
+      images: ['any_image'],
+      highlightImage: 'any_highlight_image',
+      createdAt: new Date(),
+      price: 100,
+      owner: {
+        email: 'any_email',
+        name: 'any_name'
+      }
+    }
+  ]
 }
 
 describe('House Mongo Repository', () => {
@@ -13,6 +63,7 @@ describe('House Mongo Repository', () => {
 
   beforeEach(async () => {
     await AccountMongooseModel.deleteMany()
+    await HouseMongooseModel.deleteMany()
   })
 
   afterAll(async () => {
@@ -49,6 +100,25 @@ describe('House Mongo Repository', () => {
       expect(createdHouse.name).toBe('any_name')
       expect(createdHouse.city).toBe('any_city')
       expect(createdHouse.address).toEqual({
+        houseNumber: 0,
+        street: 'any_street',
+        zipCode: 'any_zipCode'
+      })
+    })
+  })
+
+  describe('loadAll()', () => {
+    test('should return houses on success', async () => {
+      const sut = makeSut()
+      await HouseMongooseModel.insertMany(makeFakeHouses())
+      const houses = await sut.loadAll()
+      expect(houses).toBeTruthy()
+      expect(houses.length).toBe(2)
+      expect(houses[0].name).toBe('any_name_1')
+      expect(houses[0].city).toBe('any_city_1')
+      expect(houses[1].name).toBe('any_name_2')
+      expect(houses[1].city).toBe('any_city_2')
+      expect(houses[0].address).toEqual({
         houseNumber: 0,
         street: 'any_street',
         zipCode: 'any_zipCode'
